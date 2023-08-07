ec8c63d587673e8d830bcaba0522c7eab0a5412e
Joseph Wright <joseph.wright@morningstar2.co.uk>
2023-08-07 10:33:25 +0100

    Correct "doc" target setup

--- build.lua
+++ build.lua
@@ -2,10 +2,8 @@
 module = "gitinfo2"
   sourcefiles  = {"*.sty"}
   typesetfiles = {"gitinfo2.tex"}
-  textfiles    = {
-                  "gitPseudoHeadInfo.gin",
-                  "post-xxx-sample.txt",
-                 }
+  docfiles     = {"gitHeadLocal.gin"}
+  textfiles    = {"post-xxx-sample.txt"}
   demofiles    = {"gitinfotest.tex"}
   readmefiles  = {"README"}
   typesetexe   = "lualatex"
