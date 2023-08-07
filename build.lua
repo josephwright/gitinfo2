0c95cd39a0880354df597a27ee6358bd94d74af5
Joseph Wright <joseph.wright@morningstar2.co.uk>
2023-08-07 10:45:59 +0100

    Tidy build.lua

--- build.lua
+++ build.lua
@@ -1,8 +1,11 @@
 -- Build script for gitinfo2
 module = "gitinfo2"
-  sourcefiles  = {"*.sty"}
-  typesetfiles = {"gitinfo2.tex"}
-  docfiles     = {"gitHeadLocal.gin"}
-  demofiles    = {"gitinfotest.tex"}
-  typesetexe   = "lualatex"
-  packtdszip   = true
+
+demofiles    = {"gitinfotest.tex"}
+docfiles     = {"gitHeadLocal.gin"}
+sourcefiles  = {"*.sty"}
+typesetfiles = {"gitinfo2.tex"}
+
+typesetexe   = "lualatex"
+
+packtdszip   = true
